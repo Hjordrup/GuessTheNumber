890ef704c99f1f3de33765b78605762a48ee8aa7
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-26 16:51:03 +0100

    Added a simple GUI to test With

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
+
+    @FXML
+    private TextField bestScoreField;
+
+    @FXML
+    private TextField indputField;
+
+    @FXML
+    private Button playAgainBT;
+
+    @FXML
+    private Button guessBT;
+
+    @FXML
+    private Button giveUpBT;
+
+    @FXML
+    private Label answerField;
+
 }
