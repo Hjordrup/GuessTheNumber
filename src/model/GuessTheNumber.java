8dccd4edc7b7de69783c4619632205a12b9ef53a
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-26 22:42:14 +0100

    Added a observer to the model class

--- src/model/GuessTheNumber.java
+++ src/model/GuessTheNumber.java
@@ -1,15 +1,49 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GuessTheNumber {
 
     Random r1 = new Random();
-    int num = r1.nextInt(101);
+    private int num;
+    private final ArrayList<NumObserver> COLLECTIONOFNUMOBSERVERS = new ArrayList<>();
+
+    GuessTheNumber(){
+        randomNum();
+        notifyNumObserver();
+    }
 
 
+    public void randomNum(){
+        this.num = r1.nextInt(101);
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNewNum() {
+        this.num = r1.nextInt(101);
+    }
 
 
+    public void notifyNumObserver(){
+        for (NumObserver observer :COLLECTIONOFNUMOBSERVERS
+             ) { observer.update();
+        }
+    }
+
+
+    public void addNumObserver(NumObserver numObserver){
+        if(!COLLECTIONOFNUMOBSERVERS.contains(numObserver))
+        COLLECTIONOFNUMOBSERVERS.add(numObserver);
+    }
+
+
+    public interface NumObserver{
+        void update();
+    }
 
 
 }
